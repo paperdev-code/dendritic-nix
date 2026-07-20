46c44c283d43868a1ba1eec3d9fe7ddca487ea4d
jorn <git@jorn.software>
2026-07-20 22:11:03 +0200

    add pywalfox for firefox theme sync
    requires an addon on the other side, which i currently do not keep
    declarative yet.

--- modules/desktop/noctalia.nix
+++ modules/desktop/noctalia.nix
@@ -5,16 +5,21 @@
   ...
 }:
 {
-  modules.noctalia.nixos = {
+  modules.noctalia.nixos = { pkgs, ... }: {
     imports = [
       inputs.noctalia.nixosModules.default
       inputs.noctalia-greeter.nixosModules.default
+    ];
+
+    environment.systemPackages = [
+      pkgs.pywalfox-native
     ];
 
     programs.noctalia = {
       enable = true;
       recommendedServices.enable = true;
     };
+
     programs.noctalia-greeter.enable = true;
     programs.dconf.enable = true;
 
