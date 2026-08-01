f44f254c41838efe2a6d63f9ad6db2a4d0d90649
jorn <git@jorn.software>
2026-08-01 15:05:48 +0200

    nixos stubs import hjem stubs

--- modules/stubs.nix
+++ modules/stubs.nix
@@ -1,5 +1,10 @@
+{ config, ... }:
 {
   # used for namespaces where options aren't applied.
   # for ex. persistence on systems without persistence.
-  modules.stubs.nixos = { };
+  modules.stubs.nixos = {
+    hjem.extraModules = [ config.modules.stubs.hjem ];
+  };
+
+  modules.stubs.hjem = { };
 }
