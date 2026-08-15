2e2a5e2573d1fd6d878af6b23d3312f919f8563b
jorn <git@jorn.software>
2026-08-15 12:24:15 +0200

    add proton-ge

--- modules/programs/steam.nix
+++ modules/programs/steam.nix
@@ -1,8 +1,13 @@
 { config, ... }:
 {
-  modules.steam.nixos = {
+  modules.steam.nixos = { pkgs, ... }: {
     hjem.extraModules = [ config.modules.steam.hjem ];
-    programs.steam.enable = true;
+    programs.steam = {
+      enable = true;
+      extraCompatPackages = [
+        pkgs.proton-ge-bin
+      ];
+    };
   };
 
   modules.steam.hjem = {
